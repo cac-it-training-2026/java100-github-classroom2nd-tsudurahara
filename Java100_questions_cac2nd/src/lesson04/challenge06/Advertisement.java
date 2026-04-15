65960dcb6753f99605031220ef734a2e40a43870
hiwashi <hiwashi@C010422985.zipangu.cac.co.jp>
2026-04-15 16:40:14 +0900

    lesson1の残りとlesson2

---
+++ Java100_questions_cac2nd/src/lesson04/challenge06/Advertisement.java
@@ -0,0 +1,13 @@
+package lesson04.challenge06;
+
+public class Advertisement {
+
+	public static void main(String[] args) {
+		System.out.println("一緒に戦ってくれる仲間を探しています！");
+		System.out.println("\n勤務時間\t 月～金:20:30～22:00 土日:10:00～22:00");
+		System.out.println("時給\t\t 2500VP");
+		System.out.println("待遇\t\t 衣食住提供　育成用VP及び持ち物支給");
+
+	}
+
+}
